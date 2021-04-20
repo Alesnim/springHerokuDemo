1e25fb93dc9db5e4312cb75925bcd602e3ae2bd5
Aleksey <alesnim@gmail.com>
2021-04-20 18:00:26 +0300

    change cat controller

--- src/main/java/com/example/demo/WebClientConfig.java
+++ src/main/java/com/example/demo/WebClientConfig.java
@@ -21,7 +21,7 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClientWithTimeout() {
-        final var tcpClient = TcpClient
+        final TcpClient tcpClient = TcpClient
                 .create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
                 .doOnConnected(connection -> {
