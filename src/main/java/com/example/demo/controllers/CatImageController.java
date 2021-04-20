bd500805dc3b50d48423608d217496fc589f457e
Aleksey <alesnim@gmail.com>
2021-04-20 23:46:54 +0300

    change cat controller

--- src/main/java/com/example/demo/controllers/CatImageController.java
+++ src/main/java/com/example/demo/controllers/CatImageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -21,6 +22,7 @@ public class CatImageController {
     CatGetService catGetService;
     @Autowired
     CatImageRepo catImageRepo;
+
 
     @CrossOrigin
     @GetMapping("/cats")
@@ -35,6 +37,21 @@ public class CatImageController {
 
         return StreamSupport.stream(catImageRepo.findAll().spliterator(), false)
                 .collect(Collectors.toList());
+    }
+
+
+    @CrossOrigin
+    @GetMapping("/cat/random")
+    public @ResponseBody
+    CatImage getRandomCat() {
+        Random random = new Random();
+        List<CatImage> imgs = StreamSupport
+                .stream(catImageRepo
+                        .findAll()
+                        .spliterator(), false)
+                .collect(Collectors.toList());
+
+        return imgs.get(random.nextInt(imgs.size()));
     }
 
 }
--- src/main/java/com/example/demo/repository/CatImageRepo.java
+++ src/main/java/com/example/demo/repository/CatImageRepo.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface CatImageRepo extends CrudRepository<CatImage, Long> {
 
     CatImage findCatImageById(long id);
+
 }
